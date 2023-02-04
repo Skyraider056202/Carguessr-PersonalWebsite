8f349c2d17abf616821e6fcb906d343c9a053a20
Skyraider056202 <111151685+Skyraider056202@users.noreply.github.com>
2023-02-04 18:31:18 -0500

    Update pyt_back.py
    modified the code to make it take not use KelleyBlueBook and only Wikipedia, which allowed me to host it on pythonanywhere at carguessr.pythonanywhere.com

--- JS_Basics/pyt_back.py
+++ JS_Basics/pyt_back.py
@@ -5,55 +5,90 @@ import re
 import string
 from bs4 import BeautifulSoup
 import json
+from lists import chosen_list
 class game_infrastructure():
-    def __init__(self, chosen_letter, kbb_url, wiki_link, car_model, car_make, info_dict, car_data_table, image) -> None: # for every run of the game, initialize these variables.
-            self.chosen_letter = chosen_letter
-            self.kbb_url = kbb_url
+    def __init__(self, wiki_link, info_dict, car_data_table, image, a_list, chosen_data, result) -> None: # for every run of the game, initialize these variables.
+           
+            self.result = result
             self.wiki_link = wiki_link
-            self.car_model = car_model
-            self.car_make = car_make
             self.info_dict = info_dict
             self.car_data_table = car_data_table
             self.image = image
-    @classmethod
-    def list_of_letters(self):
-        list_of_letters_lowercase = list(string.ascii_lowercase)
-        self.chosen_letter = random.choice(list_of_letters_lowercase)
-        return self.chosen_letter #the Kelley Blue Book reference uses an A-Z setup to find cars, so it chooses a random letter
-    @classmethod
-    def kbb_search(self):
-        self.kbb_url = requests.get(f'https://www.kbb.com/car-make-model-list/used/{self.chosen_letter}/model/')
-        soup = BeautifulSoup(self.kbb_url.text, 'html.parser')
-        table = soup.find("tbody",{"class":"css-ap66tw ee33uo34"}) #finds the massive table with car makes, models
-        tab = table.findAll('tr')
-        link = random.choice(tab) # chooses a random row
-        car_model_html = link.find('div', {"class":"css-z687n ee33uo36"})
-        self.car_model = car_model_html.text
-        car_make_html = car_model_html.find_next() #car makes are displayed following the model.
-        self.car_make = car_make_html.text
-        self.car_model = self.car_model.replace(' ',  '_')
-        self.car_model = self.car_model.replace('-',  '_')
-        self.car_make = self.car_make.replace(' ', '_')
-        if len(self.car_make) > 3:
-            self.car_make = string.capwords(self.car_make, sep='_') #meant to exclude abbreviation makes like BMW, AMG
-        if len(self.car_model) > 3: 
-            self.car_model = string.capwords(self.car_model, sep='_') # same with model names like X5M
-        else:
-            return self.car_make, self.car_model
+            self.a_list = a_list
+            self.chosen_data = chosen_data
     
     @classmethod
+    def find_car(self):
+        self.a_list = []
+        exclusions = ['Category', 'ISBN', 'Template', 'Special', 'Wikipedia', 'http', 'foundation', 'Help', 'File']
+        url = requests.get(chosen_list).text
+        soup = BeautifulSoup(url, "html.parser")
+        if soup.findAll("tr"):
+            tabledata = soup.findAll("tr")
+            for table in tabledata:
+                a_tag = table.find_all("a")
+                if a_tag:   
+                    for link in a_tag:
+                        if 'href' in link.attrs:
+                            href = link['href']
+                            if '/wiki' in href and not any(excl in href for excl in exclusions):
+                                self.a_list.append(href) 
+                            else:
+                                continue
+                        print(link)
+            print('found tables!')  
+        else:
+            tabledata = soup.findAll("li")
+            for list in tabledata:
+                a_tag = list.find_all("a")
+                if a_tag:   
+                    for link in a_tag:
+                        if 'href' in link.attrs:
+                            href = link['href']
+                            if '/wiki' in href and not any(excl in href for excl in exclusions):
+                                self.a_list.append(href) 
+                            else:
+                                continue
+            
+            print('found lists!')
+        self.chosen_data = random.choice(self.a_list)
+        return self.chosen_data
+
+
+
+
+    @classmethod
+    def verify_model(self):
+        self.result = game_infrastructure.find_car()
+        url2 = requests.get(f'https://en.wikipedia.org{self.result}').text
+        soup2 = BeautifulSoup(url2, 'html.parser')
+        
+        try:
+            infobox = soup2.find("table", {'class': 'infobox hproduct'})
+            for table in infobox:
+                        rows = table.findChildren(['th', 'tr'])
+                        for row in rows:
+                            titles = row.findChildren('th')
+                            while 'Products' in titles or 'Product type' in titles:
+                                 game_infrastructure.verify_model()
+            self.target_car = soup2.find("th", {'infobox-above fn'}).text
+            print(self.target_car)
+            print('woo' + self.result)
+            return self.target_car
+
+        except TypeError:
+             game_infrastructure.verify_model()
+        return self.target_car, self.result     
+    @classmethod
     def wiki_search(self):
-        self.target_car = f'{self.car_make} {self.car_model}'
-        print(self.target_car)
-        self.target_car2 = re.sub(r'([a-z])([A-Z])', r'\1 \2', self.target_car)  #This is the car name that the program has selected.
         self.info_dict = {} # the dictionary where all of the car info goes
-        self.wiki_link = requests.get(f'https://en.wikipedia.org/wiki/{self.car_make}_{self.car_model}')
+        self.wiki_link = requests.get(f'https://en.wikipedia.org{self.result}')
         game_infrastructure.soup1 = BeautifulSoup(self.wiki_link.text, 'html.parser')
         game_infrastructure.car_data_table = game_infrastructure.soup1.find("table", {'class': 'infobox hproduct'})
 
     @classmethod
     def image_search(self):
-        self.image_link = requests.get(f'https://www.google.com/search?q={self.car_make}+{self.car_model}&sxsrf=ALeKk03xBalIZi7BAzyIRw8R4_KrIEYONg:1620885765119&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjv44CC_sXwAhUZyjgGHSgdAQ8Q_AUoAXoECAEQAw&cshid=1620885828054361')
+        self.image_link = requests.get(f'https://www.google.com/search?q={self.target_car}&sxsrf=ALeKk03xBalIZi7BAzyIRw8R4_KrIEYONg:1620885765119&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjv44CC_sXwAhUZyjgGHSgdAQ8Q_AUoAXoECAEQAw&cshid=1620885828054361')
         game_infrastructure.soup2 = BeautifulSoup(self.image_link.content, 'html.parser')
         self.image = game_infrastructure.soup2.find("img", {"class" : "yWs4tf"}).get('src')
         game_infrastructure.info_dict['image'] = game_infrastructure.image
@@ -87,16 +122,12 @@ class game_infrastructure():
     
     def main():
         try:
-            game_infrastructure.list_of_letters()
-            game_infrastructure.kbb_search()
+            game_infrastructure.verify_model()
             game_infrastructure.wiki_search()
             game_infrastructure.data_gathering()
             game_infrastructure.image_search()
-            while 'This article needs additional citations for verification' in game_infrastructure.car_data_table.text or 'Wikipedia does not have an article with this exact name' in game_infrastructure.car_data_table.text:
-                game_infrastructure.kbb_search() # keep rerunning the program until it lands on a good car
-                game_infrastructure.wiki_search()
-                game_infrastructure.data_gathering()
-                game_infrastructure.image_search()
-        except TypeError or AttributeError:
+            #while 'This article needs additional citations for verification' in game_infrastructure.car_data_table.text or 'Wikipedia does not have an article with this exact name' in game_infrastructure.car_data_table.text:
+               
+        except  AttributeError:
             game_infrastructure.main()
 player_1 = game_infrastructure.main()
